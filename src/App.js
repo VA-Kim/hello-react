9229b6635299e69152bc97cf1ef04505055c977a
aviewa <icewkd@gmail.com>
2019-04-12 17:39:15 +0900

    chapter3 complete

--- src/App.js
+++ src/App.js
@@ -4,33 +4,9 @@ import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
-    const text = 'Are you Awesome?';
-    const condition = true;
-    const style = {
-      backgroundColor: 'gray',
-      border: '1px solid black',
-      height: Math.round(Math.random() * 300) + 50,
-      width: Math.round(Math.random() * 300) + 50,
-      WebkitTransition: 'all',
-      MozTransition: 'all',
-      msTransition: 'all'
-    }
-
     return (
-      <div className="my-div">
-        { /* 요소 밖에서의 주석 사용법 */}
-        <h1>리액트 안녕!</h1>
-        <h1>{text}</h1>
-        {
-          condition ? 'true' : 'false'
-        }
-        <div style={style}></div>
-        <div 
-          // self-closed 태그에서만 작동하는 주석이다.
-          // 마지막 /> 가 줄바꿈하여 새로운 줄에서 작성되어야 한다.
-          /**  */
-        />
-        <MyComponent/>
+      <div>
+        <MyComponent name='react' age={3}/>
       </div>
     );
   }
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+    static defaultProps = {
+        name: 'default name'
+    }
+
+    static propsTypes = {
+        name: PropTypes.string, // name type을 문자열로 설정.
+        age: PropTypes.number.isRequired
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        }
+    }
+
     render() {
         return (
             <div>
-                나의 새롭고 멋진 컴포넌트
+                <p>Hello, my name is {this.props.name}!</p>
+                <p>I'm {this.props.age}!</p>
+                <p>The number is {this.state.number}!</p>
+                <button onClick={() => {
+                        this.setState({
+                            number: this.state.number + 1
+                        })
+                    } 
+                }>Increase Number</button>
             </div>
         );
     }
