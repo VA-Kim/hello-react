15f98e7fff986b6eaebdb785b0473326db4eca4a
VA-Kim <icewkd@gmail.com>
2019-04-19 22:52:28 +0900

    chapter4 complete

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,7 +3,34 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
 
     state = {
-        message: ''
+        message: '',
+        username: ''
+    }
+
+    // constructor(props) {
+    //     super(props);
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleClick = this.handleClick.bind(this);
+    // }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value // 각 이벤트의 속성에, 각각의 입력값을 매칭시켜준다. 
+        });
+    }
+
+    handleClick = () => {
+        alert(this.state.username + ': ' + this.state.message);
+        this.setState({
+            message: '',
+            username: ''
+        });
+    }
+
+    hanbleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            this.handleClick();
+        }
     }
 
     render() {
@@ -12,25 +39,20 @@ class EventPractice extends Component {
                 <h1>Practice Event</h1>
                 <input
                     type="text"
+                    name="username"
+                    placeholder="Put username"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
                     name="message"
                     placeholder="Put anything"
                     value={this.state.message}
-                    onChange={
-                        (e) => {
-                            this.setState({
-                                message: e.target.value
-                            });
-                        }
-                    }
-                />
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        });
-                    }
-                }>Check the state</button>
+                    onChange={this.handleChange}
+                    onKeyPress={this.hanbleKeyPress}
+                />                
+                <button onClick={this.handleClick}>Check the state</button>
             </div>
         );
     }
