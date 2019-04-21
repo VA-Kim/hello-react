c9948821972ab3f6c3b27c3a94599b33a2daf71f
VA-Kim <icewkd@gmail.com>
2019-04-21 11:31:46 +0900

    Chapter5 validation code complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <ValidationSample/>
         {/* <MyComponent name='react' age={3}/> */}
-        <EventPractice/>
+        {/* <EventPractice/> */}
+
       </div>
     );
   }
